678602b0b4f8443a62737da8515b3e4894605ea1
mike <michaelmensah56211@gmail.com>
2023-07-16 10:27:21 +0000

    initial commit

--- app/src/main/java/com/example/uitest/MainActivity.kt
+++ app/src/main/java/com/example/uitest/MainActivity.kt
@@ -3,7 +3,7 @@ package com.example.uitest
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import com.example.uitest.screens.MainScreen
+import com.example.uitest.screens.HistoryScreen
 import com.example.uitest.ui.theme.UITestTheme
 
 class MainActivity : ComponentActivity() {
@@ -11,7 +11,7 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             UITestTheme {
-                MainScreen()
+                HistoryScreen()
             }
         }
     }
